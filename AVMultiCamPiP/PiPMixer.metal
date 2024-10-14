16c307c50bc61a946b9350a966708c39021d1562
Apple <>
2024-10-14 15:32:36 +0000

    Republish sample code project.

--- AVMultiCamPiP/PiPMixer.metal
+++ AVMultiCamPiP/PiPMixer.metal
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 Shader that renders two input textures with one as a PiP and the other full screen.
--- AVMultiCamPiP/Utilities.swift
+++ AVMultiCamPiP/Utilities.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 Utilities
@@ -82,7 +82,7 @@ extension AVCaptureConnection {
 	}
 }
 
-extension AVCaptureSession.InterruptionReason: CustomStringConvertible {
+extension AVCaptureSession.InterruptionReason: @retroactive CustomStringConvertible {
 	public var description: String {
 		var descriptionString = ""
 		
